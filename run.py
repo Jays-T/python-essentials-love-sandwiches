8c02076d9ed3067c3dc8af4ec0f1bfbaa5cf1e67
John Traas <j.j.h.traas@gmail.com>
2021-08-31 15:54:17 +0200

    refactor get_sales_data + start_program

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from update_worksheets import update_sales_worksheet
+from update_worksheets import update_sales_worksheet, calculate_surplus_data
 from validators import validate_length, convert_to_int
 
 
@@ -33,22 +33,6 @@ Yes = go to step 2
 end program
 """
 
-def welcome():
-    """
-    Welcome message
-    """
-    print("Welcome to love sandwiches\n")
-    print("Update your daily sales totals,\ncalculate surplus numbers.")
-    print("Calculate the daily average sales over the last 5 market days,")
-    print("and get stock recommendations")
-    begin_program = input("Write 'start' and then hit enter to begin.... or hit enter to close the program\n")
-
-    if begin_program == "start":
-        start_program()
-    else:
-        print("Alright, we'll calculate the stock some other time.")
-        return
-
 
 def start_program():
     """
@@ -56,29 +40,15 @@ def start_program():
     """
     sales_data = get_sales_data()
 
-    """
-    Should be six values
-    """
-    is_six = validate_length(sales_data)
-    if not is_six:
-        start_program()
-        return
-    
-    """
-    Should be able to convert str to int
-    """
-    sales_data_as_int = convert_to_int(sales_data)
-    if not sales_data_as_int:
-        start_program()
-        return
-    
-    print(f"Your sales data is: {sales_data_as_int}")
+    print(f"Your sales data is: {sales_data}")
     print("Is this correct? ")
     check_input = input("Write 'yes' and hit enter to update sales data,\n Write 'no', 'cancel' or leave blank and hit enter to start again...\n")
     if check_input == "yes":
-        update_sales_worksheet(sales_data_as_int)
+        update_sales_worksheet(sales_data)
     else:
         start_program()
+    
+    calculate_surplus_data(sales_data)
 
 
 
@@ -88,13 +58,37 @@ def get_sales_data():
     Data must be six values
     Values must be convertible to Int
     """
-    print("Data must be six numbers separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Data must be six numbers separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: \n")
-    data_split = data_str.split(',')
+        data_str = input("Enter your data here: \n")
+        data_split = data_str.split(',')
+        """
+        Should be six values,
+        Should be able to convert str to int
+        """
+        if validate_length(data_split):
+            if convert_to_int(data_split):
+                break
+
     return data_split
 
 
+def main():
+    """
+    Welcome message
+    """
+    print("Welcome to love sandwiches\n")
+    print("Update your daily sales totals,\ncalculate surplus numbers.")
+    print("Calculate the daily average sales over the last 5 market days,")
+    print("and get stock recommendations\n")
+    begin_program = input("Write 'start' and then hit enter to begin.... or hit enter to close the program\n")
 
-welcome()
+    if begin_program == "start":
+        start_program()
+    else:
+        print("Alright, we'll calculate the stock some other time.")
+        return
+
+main()
